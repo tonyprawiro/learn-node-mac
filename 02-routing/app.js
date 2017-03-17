432f065a87adf490e0f1c4c0a1fbda5f56a359af
Tony Hadimulyono <tony_hadimulyono@ida-gds.com>
2017-03-17 20:44:57 +0800

    learn routing

--- 02-routing/app.js
+++ 02-routing/app.js
@@ -1,8 +1,26 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.get('/singapore', function(request, response) {
-  response.send("Welcome to Singapore");
+// Simple string literal route expression
+app.get("/singapore/:name", function(request, response) {
+  console.log(request.params);
+  response.send("Welcome to Singapore, " + request.params.name);
+});
+
+// Simple regex route expression
+app.get(/\/america/i, function(request, response) {
+  response.send("Welcome to America");
+});
+
+// Regex with numbered parameter
+app.get(/^\/russia\/(\d+)$/i, function(request, response) {
+  response.send("Welcome to Russia, Agent #" + parseInt(request.params[0]));
+});
+
+// Regex with named parameter
+app.get(/^\/:country(Thailand|Taiwan)$/, function(request, response) {
+  response.send("Let's go to " + request.params.country);
 });
 
 app.use(function(request, response) {
